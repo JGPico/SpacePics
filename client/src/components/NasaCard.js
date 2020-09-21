173cb3603685d5ba013336820594dea14ed0d3c5
JGPico <minq_2@hotmail.com>
2020-09-21 11:02:21 -0600

    Changed loading screen

--- client/src/components/NasaCard.js
+++ client/src/components/NasaCard.js
@@ -5,12 +5,12 @@ const NasaCard = (props) => {
     return (
         <div className="cardBit">
 
-            {props.title ? <h1>Title: {props.title}</h1> : <h1>Invalid Date</h1>}
+            {props.title ? <h1>Title: {props.title}</h1> : <h1></h1>}
             {props.date ? <p>Date: {props.date}</p> : <p></p>}
 
             <div className="innerBox">
                 {props.explanation ? <p>{props.explanation}</p> :
-                    <p>The Nasa API doesn't have an image for this date</p>}
+                    <p>Loading ...</p>}
                 <div className="imgHolder">
                     {props.loading ? <img src={props.url} alt="Image not available"></img> : <BeatLoader loading />}
                 </div>
